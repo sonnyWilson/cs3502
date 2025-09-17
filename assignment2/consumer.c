c3531321a78a12006b1377227687b3d2e297ff82
sonnyWilson <sonnywilson43@gmail.com>
2025-09-17 16:25:34 -0400

    sig enhancemnt

--- assignment2/consumer.c
+++ assignment2/consumer.c
@@ -4,47 +4,72 @@
 #include <string.h>
 #include <getopt.h>
 #include <time.h>
+#include <signal.h>
+
+volatile sig_atomic_t shutdown_flag = 0;
+volatile sig_atomic_t stats_flag = 0;
+
+void handle_sigint(int sig) {
+    shutdown_flag = 1;
+}
+
+void handle_sigusr1(int sig) {
+    stats_flag = 1;
+}
 
 int main(int argc, char *argv[]) {
-    int max_lines = -1;
-    int verbose = 0;   
-    int opt;            
-    char line[4096];    
-
-    
-while ((opt = getopt(argc, argv, "n:vh")) != -1) {
-switch (opt) {
-case 'n':max_lines = atoi(optarg);
-if (max_lines <= 0) {
-fprintf(stderr, "Error: max_lines must be positive\n");
-exit(1);}break;
-case 'v':verbose = 1;break;
- case 'h':
- fprintf(stderr, "Usage: %s [-n max_lines] [-v] [-h]\n", argv[0]);
- fprintf(stderr, "  -n: Maximum number of lines to process\n");
- fprintf(stderr, "  -v: Enable verbose mode\n");
- fprintf(stderr, "  -h: Show this help message\n");
- exit(0);
- default:
- fprintf(stderr, "Usage: %s [-n max_lines] [-v] [-h]\n", argv[0]);exit(1);}}
-    clock_t start = clock();
+    int max_lines = -1;  // -1 means unlimited
+    int verbose = 0;
+    char opt;
+    char line[4096];
     int line_count = 0;
     size_t char_count = 0;
-    while (max_lines == -1 || line_count < max_lines) {
-    if (fgets(line, sizeof(line), stdin) == NULL) {
- break;}
- line_count++;
- char_count += strlen(line);
- if (verbose) {fprintf(stderr, "Line %d: %s", line_count, line);}
+    while ((opt = getopt(argc, argv, "n:vh")) != -1) {
+        switch (opt) {
+            case 'n':
+                max_lines = atoi(optarg);
+                break;
+            case 'v':
+                verbose = 1;
+                break;
+            case 'h':
+            default:
+                fprintf(stderr, "Usage: %s [-n max_lines] [-v]\n", argv[0]);
+                exit(1);
+        }
     }
-    clock_t end = clock();
-    double elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
-    fprintf(stderr, "Total Lines: %d\n", line_count);
-    fprintf(stderr, "Total Characters: %zu\n", char_count);
-    if (elapsed > 0) {
-        fprintf(stderr, "Throughput: %.2f lines/sec\n", (double)line_count / elapsed);
-    } else {
-        fprintf(stderr, "Throughput: N/A (elapsed time too small)\n");
-    }
+    struct sigaction sa;
+    sa.sa_handler = handle_sigint;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+
+    sa.sa_handler = handle_sigusr1;
+    sigaction(SIGUSR1, &sa, NULL);
+
+    clock_t start_time = clock();
+    while ((max_lines == -1 || line_count < max_lines) && !shutdown_flag) {
+        if (fgets(line, sizeof(line), stdin) == NULL) break;
+        line_count++;
+        char_count += strlen(line);
+        if (verbose) {
+            printf("%s", line);
+            fflush(stdout);
+        }
+        if (stats_flag) {
+            clock_t now = clock();
+            double elapsed = ((double)(now - start_time)) / CLOCKS_PER_SEC;
+            if (elapsed > 0) {
+    fprintf(stderr, "Consumer stats: %d lines, %.2f lines/sec\n", line_count,
+                        (double)line_count / elapsed);
+            }
+            stats_flag = 0;}}
+    clock_t end_time = clock();
+    double elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    if (elapsed_time > 0) {
+        double throughput_lines_s = (double)line_count / elapsed_time;
+        fprintf(stderr, "Lines: %d\n", line_count);
+        fprintf(stderr, "Characters: %zu\n", char_count);
+fprintf(stderr, "Time: %.2f sec, Throughput: %.2f lines/sec\n", elapsed_time, throughput_lines_s);}
     return 0;
 }
--- assignment2/producer.c
+++ assignment2/producer.c
@@ -3,33 +3,87 @@
 #include <unistd.h>
 #include <string.h>
 #include <getopt.h>
+#include <signal.h>
+#include <time.h>
+
+volatile sig_atomic_t shutdown_flag = 0;
+volatile sig_atomic_t stats_flag = 0;
+
+void handle_sigint(int sig) {
+    shutdown_flag = 1;
+}
+
+void handle_sigusr1(int sig) {
+    stats_flag = 1;
+}
 
 int main(int argc, char *argv[]) {
-	FILE *input=stdin;
-	int buffer_size = 4096;
-	char *filename = NULL;
-	char opt;
-while ((opt = getopt(argc, argv,"f:b:")) != -1){
-
-switch (opt){
-case 'f': filename =optarg; break;
-case 'b': buffer_size = atoi(optarg); break;
- default: fprintf(stderr, "Usage: %s [-f file] [-b size]\n", argv[0]);
-  exit(1);
-  }}
-
-  if (filename != NULL){
-  input = fopen(filename, "r");
-  if (input == NULL){ perror("invalid");
-  exit(1);
-  }}
-
-char *buffer = malloc(buffer_size);
-size_t bytes_read;
-while((bytes_read = fread(buffer, 1,buffer_size,input)) >0){
-if (fwrite(buffer, 1, bytes_read,stdout)!= bytes_read){ perror("faild"); break;}
-fflush(stdout);}
-free(buffer); if (input!= stdin){
-fclose(input);}
-	return 0;
-	}
+    FILE *input = stdin;
+    int buffer_size = 4096;
+    char *filename = NULL;
+    int opt;
+    size_t total_bytes = 0;
+    clock_t start_time = clock();
+    
+    struct sigaction sa = {0};
+    sa.sa_handler = handle_sigint;
+    sigaction(SIGINT, &sa, NULL);
+    sa.sa_handler = handle_sigusr1;
+    sigaction(SIGUSR1, &sa, NULL);
+    while ((opt = getopt(argc, argv, "f:b:h")) != -1) {
+        switch (opt) {
+            case 'f':
+                filename = optarg;
+                break;
+            case 'b':
+                buffer_size = atoi(optarg);
+                if (buffer_size <= 0) {
+                    fprintf(stderr, "Error: buffer_size must be positive\n");
+                    exit(1);
+                }
+                break;
+            case 'h':
+                fprintf(stderr, "Usage: %s [-f filename] [-b buffer_size][-h]\n", argv[0]);
+                fprintf(stderr,"-f: Input file (default: stdin)\n");
+                fprintf(stderr,"-b: Buffer size in bytes (default: 4096)\n");
+                fprintf(stderr,"-h: Show this help message\n");
+                exit(0);
+            default:
+                fprintf(stderr, "Usage: %s [-f filename] [-b buffer_size][-h]\n", argv[0]);
+                exit(1);
+        }}
+    if (filename != NULL) {
+        input = fopen(filename, "r");
+        if (input == NULL) {
+            perror("Failed to open input file");
+            exit(1);
+        }
+    }
+    char *buffer = malloc(buffer_size);
+    if (buffer == NULL) {
+        perror("Failed to allocate buffer");
+        if (input != stdin) fclose(input);
+        exit(1); }
+    size_t bytes_read;
+    while ((bytes_read = fread(buffer, 1, buffer_size, input)) > 0 &&
+     !shutdown_flag) {
+        if (fwrite(buffer, 1, bytes_read, stdout) != bytes_read) {
+            perror("Failed to write to stdout");
+            break; }
+        total_bytes += bytes_read;
+        if (stats_flag) {
+            clock_t now = clock();
+            double elapsed = ((double)(now - start_time)) / CLOCKS_PER_SEC;
+            if (elapsed > 0) {
+                fprintf(stderr, "Producer stats: %zu bytes, %.2f MB/s\n",
+                        total_bytes, (total_bytes / 1024.0 / 1024.0) / elapsed);
+            } else {
+                fprintf(stderr, "Producer stats: %zu bytes, N/A MB/s(elapsed time too small)\n",
+                        total_bytes);}
+            stats_flag = 0;}}
+    if (ferror(input)) {
+        perror("Failed to read input");}
+    free(buffer);
+    if (input != stdin) {
+        fclose(input);}
+    return 0;}
